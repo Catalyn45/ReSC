3000be3fc7f718f5bb2aec06757c1ca7cd1c716e
Catalyn45 <ancutei.catalin@gmail.com>
2021-03-17 00:14:34 +0200

    refactorized the code and added the minimizing function

--- front_end/experimental/assets/js/script.js
+++ front_end/experimental/assets/js/script.js
@@ -1,17 +1,46 @@
 function sendMsg() {
-    var inp = document.getElementById('input_message');
-    cont.insertAdjacentHTML('beforeend', '<div class="chat__content__me"><div class="chat__content__me__container"><p>' + inp.value + '</p></div></div>');
-    cont.scrollBy(0, 100);
-    inp.value = "";
-    inp.focus();
+    const sendMessage = `
+        <div class="chat__content__me">
+            <div class="chat__content__me__container">
+                <p>${input.value}</p>
+            </div>
+        </div>
+    `
+    content.insertAdjacentHTML('beforeend', sendMessage);
+    content.scrollBy(0, content.scrollHeight);
+    input.value = "";
+    input.focus();
     setTimeout(strangerMsg, 1000);
     return false;
 }
 
 function strangerMsg() {
-    cont.insertAdjacentHTML('beforeend', '<div class="chat__content__stranger"><div class="chat__content__stranger__container"><p>Asa este, aveti dreptate!</p></div></div>');
-    cont.scrollBy(0, 100);
+    const backMessage = `
+        <div class="chat__content__stranger">
+            <div class="chat__content__stranger__container">
+                <p>Buna ziua, numele meu este Diana. cu ce va pot ajuta?</p>
+            </div>
+        </div>
+    `
+    content.insertAdjacentHTML('beforeend', backMessage);
+    content.scrollBy(0, content.scrollHeight);
 }
 
-var cont = document.getElementById('chat_cont');
-cont.scrollBy(0, 1000);
+function hide() {
+    if (isHidden) {
+        content.style.display = 'block';
+        inputbar.style.display = 'block';
+    } else {
+        content.style.display = 'none';
+        inputbar.style.display = 'none';
+    }
+
+    isHidden = !isHidden;
+}
+
+var isHidden = true;
+var content = document.getElementById('chat__content');
+var inputbar = document.getElementById('chat__inputbar');
+var input = document.getElementById('input_message');
+
+content.scrollBy(0, content.scrollHeight);
