0ea4f277e3f07944218ef41388c27aae359695ef
Cătălin Ancuței <cancutei@bitdefender.com>
2021-03-16 14:04:21 +0200

    added automatic message back

--- front_end/experimental/assets/js/script.js
+++ front_end/experimental/assets/js/script.js
@@ -4,7 +4,13 @@ function sendMsg() {
     cont.scrollBy(0, 100);
     inp.value = "";
     inp.focus();
+    setTimeout(strangerMsg, 1000);
     return false;
+}
+
+function strangerMsg() {
+    cont.insertAdjacentHTML('beforeend', '<div class="chat__content__stranger"><div class="chat__content__stranger__container"><p>Asa este, aveti dreptate!</p></div></div>');
+    cont.scrollBy(0, 100);
 }
 
 var cont = document.getElementById('chat_cont');
