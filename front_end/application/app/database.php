1693903cfcf7b52e32c3e5171cb7437372d11c0d
Catalyn45 <ancutei.catalin@gmail.com>
2021-05-25 00:07:04 +0300

    changed database connection

--- front_end/application/app/database.php
+++ front_end/application/app/database.php
@@ -4,12 +4,15 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 $capsule = new Capsule();
 
+$DATABASE_URL = parse_url(getenv("DATABASE_URL"));
+
 $capsule->addConnection([
     'driver' => 'pgsql',
-    'host' => '192.168.1.150',
-    'username' => 'postgres',
-    'password' => 'admin',
-    'database' => 'Resc',
+    'host' => $DATABASE_URL["host"],
+    'port' => $DATABASE_URL["port"],
+    'username' => $DATABASE_URL["user"],
+    'password' => $DATABASE_URL["pass"],
+    'database' => ltrim($DATABASE_URL["path"], "/"),
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => ''
